3110a5a1914dfc5529b99ecf7dd108976b0e0d66
Anuradha0066 <singhanuradha0066@gmail.com>
2025-07-15 23:31:18 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+
+// import React from 'react'
+// import './App.css'
+
+
+// const App = () => {
+//   let arr=[1,2,3,4,5] 
+//   function fun1(){
+//     console.log("hehehehe");
+    
+//   }
+//   return (
+//     // <div>
+//     // <button  onClick={fun1}>click</button>
+//     // </div>
+//     <div>App 
+//       {
+//         arr.map((a)=>{
+//           return(<>
+//           <h1> {a}</h1>
+//           </>)
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+
+// // const Home=()=>{
+// //   return(
+// //     <div>
+// //       <h2> Home Compoo</h2>
+// //     </div>
+// //   )
+
+// // }
+// // export {Home}
+
+
+// export default App
+
+
+
+
+import React from 'react'
+import Day2 from './Day2'
+import Day3 from './Day3'
+
+const App = () => {
+  return (
+    <div>
+      <Day3/>
+    </div>
+  )
+}
+
+export default App
